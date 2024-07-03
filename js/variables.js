3704381cc5ac79bd7601a1c347459a269cf1a347
keithcampos02 <alonso826campos@gmail.com>
2024-07-02 20:23:07 -0600

    feat completando

--- js/variables.js
+++ js/variables.js
@@ -1,21 +1,14 @@
 console.log("Hola Mundo");
 
 
+let Nombre = "keith"
+let Apellido = "Campos"
+let Edad = "24"
+let Direccion = "sarchi"
 
 
 
-
-
-
-
-const nombre="keith"
-const apellido="Campos"
-let edad="24"
-let direccion= "sarchi"
-
-
-
-console.log(nombre)
-console.log(apellido)
-console.log(edad)
-console.log(direccion)
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(direccion);
