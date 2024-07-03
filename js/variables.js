1f0985477430e6b3ebe8fb5491277f7a82644d1b
keithcampos02 <alonso826campos@gmail.com>
2024-07-02 20:04:00 -0600

    feat js inicio

---
+++ js/variables.js
@@ -0,0 +1,21 @@
+console.log("Hola Mundo");
+
+
+
+
+
+
+
+
+
+const nombre="keith"
+const apellido="Campos"
+let edad="24"
+let direccion= "sarchi"
+
+
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(direccion)
