61540aa4bdc7a67921ff8e4261ca15f3e9312716
keithcampos02 <alonso826campos@gmail.com>
2024-07-04 19:25:37 -0600

    feat cambios 7/4/2024

---
+++ js/javascript2.js
@@ -0,0 +1,14 @@
+
+
+
+
+let auto ={
+
+marca: "toyota",
+modelo: 1999,
+electrico:true,
+capacidad:[1,2,3]
+
+}
+
+console.log(auto)
---
+++ js/tipodedatos.js
@@ -0,0 +1,13 @@
+console.log("Hola Mundo");
+
+// zona para declarar variables
+
+
+let Nombre = "keith"
+let Edad = "24"
+let tiene mascotas = true
+let tiene gatos = false
+let ventana 
+let mensaje2 = "es usted mayor de edad?"
+
+
